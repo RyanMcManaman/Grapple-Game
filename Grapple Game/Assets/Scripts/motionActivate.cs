e877661aeaf8e21810fb721866a3f58f3f241bd1
Isaac Logsdon <isaacjlogsdon@gmail.com>
2018-03-07 15:02:16 -0600

    Pressure Plate Implementation

---
+++ Grapple Game/Assets/Scripts/motionActivate.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class motionActivate : MonoBehaviour {
+
+	//this can be attached to any object and set to move how they want. 
+
+	Animator anime;
+	//object to be moved
+
+	public GameObject PressurePlate;      //needs to be the 'small' cube of pressure plate
+	private pressurePlate scriptAccess;
+
+	bool isPressed;
+
+	void Start ()
+	{
+		scriptAccess = PressurePlate.GetComponent<pressurePlate> ();
+		isPressed = scriptAccess.fullPress;	//can be used for activating stuff if needed
+
+		anime = (GetComponent<Animator>());
+	}
+
+	void Update () 
+	{
+		scriptAccess = PressurePlate.GetComponent<pressurePlate> ();
+
+		isPressed = scriptAccess.fullPress;
+		if (isPressed) {
+			anime.enabled = false;
+		}  else {
+			anime.enabled = true;
+		}
+
+	}
+}
+
---
+++ Grapple Game/Assets/Scripts/pressurePlate.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressurePlate : MonoBehaviour {
+
+	Animator animator;
+
+	float plateX;
+	float plateY;
+	float plateZ;
+	float plateMax;
+	float plateMin;
+
+	public Transform plate;
+
+	bool pressed;
+	public bool fullPress;
+
+	void Start () 
+	{
+		pressed = false;
+		fullPress = false;
+		animator = GetComponent<Animator>();
+
+		plateX = plate.position.x;
+		plateY = plate.position.y;
+		plateZ = plate.position.z;
+		plateMax = plateY;
+		plateMin = plateY - .024f;
+	}
+
+	void Update () 
+	{
+		if (pressed == true) //plate goes down
+		{
+
+			if (transform.position.y > plateMin) 
+			{
+				transform.position = new Vector3 (plateX, plateY, plateZ);
+				plateY -= .001f;
+			}
+			if (transform.position.y < plateMin+.0011f) {
+				fullPress = false;
+			}
+
+		}  else if(pressed == false) //plate goes up
+		{
+			fullPress = true;
+			if (transform.position.y < plateMax) 
+			{
+				transform.position = new Vector3 (plateX, plateY, plateZ);
+				plateY += .001f;
+			}  
+
+		}
+	}
+
+	void OnTriggerEnter(Collider other)   //block is on pressure plate
+	{
+		if (other.gameObject.CompareTag ("pressurePlateBox")) 
+		{
+			pressed = true;
+		}
+	}
+
+
+	void OnTriggerExit(Collider other) //block comes off pressure plate
+	{
+		if (other.gameObject.CompareTag ("pressurePlateBox")) 
+		{
+			pressed = false;
+		}
+	}
+
+
+}
