24729903d4085d90c2b13d435de360c704339974
Isaac Logsdon <isaacjlogsdon@gmail.com>
2018-03-12 16:06:06 -0500

    almostDoneish

---
+++ Grapple Game/Assets/Scripts/leverAnimeScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leverAnimeScript : MonoBehaviour 
+{
+
+
+	Animator animator;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("leverTrigger")) 
+		{
+			animator.SetBool("isTriggered",true);
+		}
+
+	}
+
+
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.CompareTag ("leverTrigger")) 
+		{
+			animator.SetBool("isTriggered",true);
+		}
+
+	}
+
+
+}
