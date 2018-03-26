69a23c6b951fa8c3b5f8186cfac0b6f9e446d73a
Zealot-311 <jackjohnson22@hotmail.com>
2018-03-26 16:19:00 -0500

    added TorchTarget and TorchMember scripts

---
+++ Grapple Game/Assets/Scripts/TorchMember.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchMember : MonoBehaviour {
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.collider.gameObject.tag.Contains("torch"))
+    {
+      if (!this.gameObject.tag.Contains("lit"))
+      {
+        this.gameObject.tag = this.gameObject.tag + " lit";
+      }
+    }
+  }
+}
---
+++ Grapple Game/Assets/Scripts/TorchTarget.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//this is an effect independent script that you can use to activate an object
+//when all torches are lit up.
+public class TorchTarget : MonoBehaviour {
+  public GameObject[] Torches;
+	// Update is called once per frame
+	void Update () {
+    int i = 0;
+		foreach(GameObject obj in Torches)
+    {
+      if (obj.tag.Contains("alight"))
+      {
+        i++;
+      }
+    }
+    if (i >= Torches.Length)
+    {
+      if (!this.gameObject.tag.Contains("activated"))
+        this.gameObject.tag = this.gameObject.tag + " activated";
+    }
+	}
+}
