cb92694db592ef129e61b23575e376463eccbad7
Isaac Logsdon <isaacjlogsdon@gmail.com>
2018-04-09 00:12:54 -0500

    area01Finished

---
+++ Grapple Game/Assets/Scripts/resetPlayer.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetPlayer : MonoBehaviour 
+{
+	float playerX;
+	float playerY;
+	float playerZ;
+
+	Transform player;
+
+
+	public GameObject box; 
+
+
+	void Start ()
+	{
+
+	
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		playerX = player.position.x;
+		playerY = player.position.y;
+		playerZ = player.position.z;
+	}
+		
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("ResetPlayer")) 
+		{
+			print ("helloifnelkfn");
+			transform.position = new Vector3 (playerX, playerY, playerZ);
+		}
+
+	}
+}
